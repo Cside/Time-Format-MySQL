f59fb8f59f514219da17f2763811b0d73c6ca17d
Hiroki Honda <cside.story@gmail.com>
2013-05-21 21:07:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Time::Format::MySQL - It's new $module
+
+# SYNOPSIS
+
+    use Time::Format::MySQL;
+
+# DESCRIPTION
+
+Time::Format::MySQL is ...
+
+# LICENSE
+
+Copyright (C) Hiroki Honda.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hiroki Honda <cside.story@gmail.com>
---
+++ lib/Time/Format/MySQL.pm
@@ -0,0 +1,39 @@
+package Time::Format::MySQL;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Time::Format::MySQL - It's new $module
+
+=head1 SYNOPSIS
+
+    use Time::Format::MySQL;
+
+=head1 DESCRIPTION
+
+Time::Format::MySQL is ...
+
+=head1 LICENSE
+
+Copyright (C) Hiroki Honda.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hiroki Honda E<lt>cside.story@gmail.comE<gt>
+
+=cut
+
