95a344bdd7f031ab77175e2b5a8aa740b3800b99
Hiroki Honda <cside.story@gmail.com>
2013-05-21 21:08:55 +0900

    0.01

--- lib/Time/Format/MySQL.pm
+++ lib/Time/Format/MySQL.pm
@@ -2,10 +2,20 @@ package Time::Format::MySQL;
 use 5.008005;
 use strict;
 use warnings;
+use Carp qw(croak);
+use Time::Piece ();
+use parent qw(Exporter);
+our @EXPORT = qw(from_unixtime unix_timestamp);
 
 our $VERSION = "0.01";
 
+sub from_unixtime {
+    Time::Piece::localtime(shift)->strftime('%Y-%m-%d %H:%M:%S');
+}
 
+sub unix_timestamp {
+    Time::Piece::localtime->strptime(shift, '%Y-%m-%d %H:%M:%S')->epoch;
+}
 
 1;
 __END__
@@ -14,15 +24,42 @@ __END__
 
 =head1 NAME
 
-Time::Format::MySQL - It's new $module
+Time::Format::MySQL - provides from_unixtime() and unix_timestamp()
 
 =head1 SYNOPSIS
 
     use Time::Format::MySQL;
 
+    print from_unixtime(time); #=> 2013-01-11 12:03:28
+    print unix_timestamp('2013-01-11 12:03:28'); #=> 1357873408
+
 =head1 DESCRIPTION
 
-Time::Format::MySQL is ...
+Time::Format::MySQL provides mysql-like functions, from_unixtime() and unix_timestamp().
+
+=head1 FUNCTIONS
+
+=over
+
+=item from_unixtime
+
+unix timestamp -> date time
+
+=item unix_timestamp
+
+date time -> unix timestamp
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item L<DateTime::Format::MySQL>
+
+=item L<Time::Piece::MySQL>
+
+=back
 
 =head1 LICENSE
 
